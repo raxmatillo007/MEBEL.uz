bc1e76cc9e4a00bd672b7e870ae0c4d1d0d714b0
raxmatillo007 <YOUR_EMAIL@gmail.com>
2025-05-27 23:16:17 +0500

    hammasi uzgardi

---
+++ mebel/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>Mebel.uz sayti</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+header {
+  background-color: #f5f5f5;
+  padding: 20px;
+  text-align: center;
+}
+</style>
